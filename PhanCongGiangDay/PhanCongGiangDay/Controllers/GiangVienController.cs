bb4be6a1c41a7f02460bf15597b00b579a8d08eb
Triet truong <truongminhtriet96@gmail.com>
2018-12-27 15:21:49 +0700

    Đang làm thêm/sửa/xóa Giảng viên

---
+++ PhanCongGiangDay/PhanCongGiangDay/Controllers/GiangVienController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lib.Setting.Model;
+using Lib.GiangVien.Model;
+using PhanCongGiangDay.IServices;
+using PhanCongGiangDay.Models.ViewModel.Shared;
+
+namespace PhanCongGiangDay.Controllers
+{
+    public class GiangVienController : BizController
+    {
+        private readonly IGiangVienService GiangVienService;
+        public GiangVienController(IGiangVienService _giangVienService)
+        {
+            GiangVienService = _giangVienService;
+        }
+        // GET: GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DanhSachGiangVien()
+        {
+            var viewModel = GiangVienService.DanhSachGiangVien();
+            return Json(new { data = viewModel }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult ThemGiangVien()
+        {
+            return PartialView("_ThemGiangVien");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThemGiangVien(GiangVienModel model)
+        {
+            try
+            {
+                var result = GiangVienService.ThemGiangVien(model);
+                if (result != null && result.ResponseCode == 1)
+                {
+                    return Json(JsonResponseViewModel.CreateSuccess("Thêm giảng viên thành công."));
+                }
+                else if (result != null && result.ResponseCode == -1)
+                {
+                    return Json(JsonResponseViewModel.CreateFail(result.ResponseMessage));
+                }
+                else
+                    return Json(JsonResponseViewModel.CreateFail("Thêm giảng viên thất bại."));
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseViewModel.CreateFail(ex));
+            }
+        }
+
+        [HttpGet]
+        public ActionResult SuaGiangVien(int id)
+        {
+            var viewModel = GiangVienService.ChiTietGiangVien(id);
+            return PartialView("_SuaGiangVien",viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SuaGiangVien(GiangVienModel model)
+        {
+            try
+            {
+                var result = GiangVienService.SuaGiangVien(model);
+                if (result != null && result.ResponseCode == 1)
+                {
+                    return Json(JsonResponseViewModel.CreateSuccess("Cập nhật giảng viên thành công."));
+                }
+                else if (result != null && result.ResponseCode == -1)
+                {
+                    return Json(JsonResponseViewModel.CreateFail(result.ResponseMessage));
+                }
+                else
+                    return Json(JsonResponseViewModel.CreateFail("Cập nhật giảng viên thất bại."));
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseViewModel.CreateFail(ex));
+            }
+        }
+
+        [HttpGet]
+        public ActionResult XoaGiangVien(int id)
+        {
+            return PartialView("_XoaGiangVien",id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult XoaGiangVienConfirmed(int GiangVienId)
+        {
+            try
+            {
+                var result = GiangVienService.XoaGiangVien(GiangVienId, "");
+                if (result != null && result.ResponseCode == 1)
+                {
+                    return Json(JsonResponseViewModel.CreateSuccess("Xoá giảng viên thành công."));
+                }
+                else if (result != null && result.ResponseCode == -1)
+                {
+                    return Json(JsonResponseViewModel.CreateFail(result.ResponseMessage));
+                }
+                else
+                    return Json(JsonResponseViewModel.CreateFail("Xoá giảng viên thất bại."));
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonResponseViewModel.CreateFail(ex));
+            }
+        }
+    }
+}
---
+++ PhanCongGiangDay/PhanCongGiangDay/Scripts/Custom/GiangVien/GiangVien.js
@@ -0,0 +1,194 @@
+﻿$(function () {
+    GiangVienModule.init();
+});
+
+var GiangVienModule = (function () {
+    var $GiangVienTable;
+
+    var GiangVienUrl = "/GiangVien/DanhSachGiangVien";
+
+    function init() {
+        loadGiangVien();
+    }
+
+    function loadGiangVien() {
+        initGiangVienDataTable();
+    }
+
+    function initGiangVienDataTable() {
+        $GiangVienTable = $("#giang-vien-table").DataTable({
+            searching: true,
+            order: [],
+            pageLength: 10,
+            lengthChange: false,
+            ajax: {
+                url: GiangVienUrl,
+                data: {
+                },
+                method: "GET",
+                beforeSend: function () {
+                    showLoadingOverlay();
+                },
+                complete: function () {
+                    hideLoadingOverlay();
+                    //initRoleNhanVien();
+                }
+            },
+
+            columns: [
+                { data: "STT" },
+                { data: "MaCB" },
+                { data: "Ho" },
+                { data: "Ten" },
+                { data: "NamSinh" },
+                { data: "ChucDanh" },
+                { data: "HocVi" },
+                { data: "ChucVu" },
+                { data: "TenDonVi" },
+                { data: "TenBoMon" },
+                { data: "Truong" },
+                {
+                    data: "GiangVienID", orderable: false, width: 140, className: "text-center", render: function (data) {
+                        return '<button class="btn btn-sm btn-outline-primary btn-custom-size btn-grid" data-trigger="modal" data-target="#modal" data-url="/GiangVien/SuaGiangVien?id=' + data + '"><i class="fa fa-pencil-square-o"></i></button>' + ' ' +
+                            '<button class="btn btn-sm btn-outline-danger btn-custom-size btn-grid" data-trigger="modal" data-target="#modal" data-url="/GiangVien/XoaGiangVien?id=' + data + '"><i class="fa fa-trash-o"></i></button>';
+                        //'<button class="btn btn-sm btn-outline-success btn-custom-size" data-trigger="modal" data-target="#modal" data-url="/GiangVien/ChiTietGiangVien?id=' + data + '"><i class="fa fa-bars"></i></button>' + ' ' +
+                    }
+                }
+            ]
+        });
+        $('#giang-vien-table_filter').hide();
+
+    }
+    function timKiem() {
+        $('#GiangVien-search').keyup(function () {
+            $GiangVienTable.search($(this).val()).draw();
+        });
+    }
+
+
+
+    function reloadGiangVienTable() {
+        $GiangVienTable.ajax.reload();
+
+    }
+
+    return {
+        init: init,
+        timKiem: timKiem,
+        reloadGiangVienTable: reloadGiangVienTable,
+
+    }
+})();
+
+var ThemGiangVienModule = (function (GiangVienModule) {
+    function init() {
+        bindFormActions();
+    }
+    function bindFormActions() {
+        $("#btn_themGiangVien").on("click",
+            function () {
+                if ($("#formThemGiangVien").valid()) {
+                    showLoadingOverlay();
+                    $.ajax({
+                        type: $("#formThemGiangVien").prop("method"),
+                        url: $("#formThemGiangVien").prop("action"),
+                        data: $("#formThemGiangVien").serialize(),
+                        success: function (response) {
+                            if (!response.IsSuccess) {
+                                $.notify({ message: response.Messages }, { type: "danger" });
+                            } else {
+                                GiangVienModule.reloadGiangVienTable();
+                                $.notify({ message: response.Messages }, { type: "success" });
+
+                            }
+                        },
+                        complete: function () {
+                            $('#modal').modal("hide");
+                            hideLoadingOverlay();
+                        }
+                    });
+                }
+                return false;
+            });
+    }
+
+    return {
+        init: init
+    }
+})(GiangVienModule);
+
+var SuaGiangVienModule = (function (GiangVienModule) {
+    function init() {
+        bindFormActions();
+    }
+    function bindFormActions() {
+        $("#btn_suaGiangVien").on("click",
+            function () {
+                if ($("#formSuaGiangVien").valid()) {
+                    showLoadingOverlay();
+                    $.ajax({
+                        type: $("#formSuaGiangVien").prop("method"),
+                        url: $("#formSuaGiangVien").prop("action"),
+                        data: $("#formSuaGiangVien").serialize(),
+                        success: function (response) {
+                            if (!response.IsSuccess) {
+                                $.notify({ message: response.Messages }, { type: "danger" });
+                            } else {
+                                GiangVienModule.reloadGiangVienTable();
+                                $.notify({ message: response.Messages }, { type: "success" });
+
+                            }
+                        },
+                        complete: function () {
+                            $('#modal').modal("hide");
+                            hideLoadingOverlay();
+                        }
+                    });
+                }
+                return false;
+            });
+    }
+
+    return {
+        init: init
+    }
+})(GiangVienModule);
+
+var XoaGiangVienModule = (function (GiangVienModule) {
+    var $formXoaGiangVien;
+
+    function init() {
+        bindFormActions();
+    }
+
+    function bindFormActions() {
+        $("#xoa-GiangVien-btn").on("click",
+            function () {
+                showLoadingOverlay();
+                $.ajax({
+                    type: $("#formXoaGiangVien").prop("method"),
+                    url: $("#formXoaGiangVien").prop("action"),
+                    data: $("#formXoaGiangVien").serialize(),
+                    success: function (response) {
+                        if (!response.IsSuccess) {
+                            $.notify({ message: response.Messages }, { type: "danger" });
+                        } else {
+                            $.notify({ message: response.Messages }, { type: "success" });
+                            GiangVienModule.reloadGiangVienTable();
+                        }
+                    },
+                    complete: function () {
+                        $('#modal').modal("hide");
+                        hideLoadingOverlay();
+                    }
+                });
+                return false;
+            });
+    }
+
+
+
+    return {
+        init: init
+    }
+})(GiangVienModule);
